0a5e446cc12e29a14a863a3d3c129b423025184a
vaniaandrienko <nekiy179@gmail.com>
2022-01-31 23:23:21 +0200

    HW Done

---
+++ JS/Lesson1/HW/main.js
@@ -0,0 +1,115 @@
+// let hello = 'hello ';
+// console.log(hello);
+// alert(hello)
+// document.write(hello+ "<br/>")
+//
+// let owu = 'owu ';
+// console.log(owu);
+// alert(owu)
+// document.write(owu+ "<br/>")
+//
+// let com = 'com ';
+// console.log(com);
+// alert(com)
+// document.write(com+ "<br/>")
+//
+// let ua = 'ua ';
+// console.log(ua);
+// alert(ua)
+// document.write(ua+ "<br/>")
+//
+// let number1 = 1;
+// console.log(number1);
+// alert(number1)
+// document.write(number1+ "<br/>")
+//
+// let number2 = 10;
+// console.log(number2);
+// alert(number2)
+// document.write(number2+ "<br/>")
+//
+// let number3 = -999;
+// console.log(number3);
+// alert(number3)
+// document.write(number3+ "<br/>")
+//
+// let number4 = 123;
+// console.log(number4);
+// alert(number4)
+// document.write(number4+ "<br/>")
+//
+// let number5 = 3.14;
+// console.log(number5);
+// alert(number5)
+// document.write(number5+ "<br/>")
+//
+// let number6 = 2.7;
+// console.log(number6)
+// alert(number6)
+// document.write(number6+ "<br/>")
+//
+// let number7 = 16;
+// console.log(number7)
+// alert(number7)
+// document.write(number7+ "<br/>")
+//
+// let true1 = 'true ';
+// console.log(true1)
+// alert(true1)
+// document.write(true1+ "<br/>")
+//
+// let folse1 = 'folse ';
+// console.log(folse1);
+// alert(folse1)
+// document.write(folse1+ "<br/>")
+
+
+// let person = {
+//     firstName : 'Ivan',
+//     middleName : 'Olegovach',
+//     lastName : 'Andrienko',
+// }
+// console.log(person);
+
+
+// let test = prompt("Вітаю Іван Олегович");
+// let test2 = prompt("Тобі 24 роки");
+
+
+// let a = 100;
+// console.log(typeof a)
+// let b = '100';
+// console.log(typeof b)
+// let c = true;
+// console.log(typeof c)
+
+
+// let a = 5 < 6;
+// console.log(a);
+// let a1 = 5 === 6;
+// console.log(a1);
+// let a2 = 5 > 6;
+// console.log(a2);
+// let a3 = 5 >= 6;
+// console.log(a3);
+//
+// let b = 10 === 10;
+// console.log(b)
+// let b1 = 10 <= 10;
+// console.log(b1)
+// let b2 = 10 > 10;
+// console.log(b2)
+// let b3 = 10 !== 10;
+// console.log(b3)
+// let b4 = 10 < 10;
+// console.log(b4)
+
+//
+// let a =(123 === '123')
+// console.log(a)
+// let b =(123 > '123')
+// console.log(b)
+
+
+
+
